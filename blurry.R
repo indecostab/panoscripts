dff97b20f5d5329f28cfb6836c8241c3ca2ffa3f
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2021-06-02 18:03:01 +0200

    Initial commit

---
+++ blurry.R
@@ -0,0 +1,64 @@
+#!/usr/bin/Rscript
+# 
+# This file will compute the blurryness index of an image
+# 
+
+suppressPackageStartupMessages( require(spatialwarnings) )
+suppressPackageStartupMessages( require(plyr) )
+suppressPackageStartupMessages( require(ggplot2) )
+
+args <- commandArgs(trailingOnly = TRUE)
+if ( length(args) < 3 ) { 
+  stop("Bad command line. Usage: blurry.R <image_dir> <input_fps> <output_fps>")
+} else { 
+  imgdir <- args[1]
+  input_fps <- as.numeric(args[2])
+  output_fps <- as.numeric(args[3])
+}
+
+all_images <- dir(imgdir, pattern = "*.png", full = TRUE)
+
+# Compute Moran lag-1 autocorrelation on all images
+cat("Scanning images...\n")
+all_blurs <- ldply(seq_along(all_images), function(imgn) { 
+  imgf <- all_images[imgn]
+  img <- png::readPNG(imgf)
+  
+  moran_base <- raw_cg_moran(img[ , ,1], 1)
+  data.frame(imgf = imgf, 
+             imgtime = imgn*(1/input_fps), 
+             moran   = moran_base)
+  
+}, .progress = "time")
+
+# Make debug plot in 
+pl <- ggplot(all_blurs) + 
+  geom_line(aes(x = imgtime, y = moran)) 
+
+ggsave(pl, file = paste0(imgdir, "_blur.pdf"), 
+       width = 16, height = 5)
+
+# Scan images and select the one with the lowest moran correlation for each 
+# second
+imgtime <- with(all_blurs, seq(min(imgtime), max(imgtime), by = output_fps))
+to_keep <- ldply(imgtime, function(imt) { 
+  d <- subset(all_blurs, imgtime > imt & imgtime < (imt+1) )
+  if ( nrow(d) == 0 ) { 
+    return( NULL )
+  }
+  data.frame(kept_imgf = subset(d, moran == min(moran))[ ,"imgf"])
+})
+
+# Create folder with filtered images 
+newf <- paste0(imgdir, "_filtered")
+if ( ! dir.exists(newf) ) { 
+  dir.create(newf)
+}
+
+# Copy the selected images into filtered folder 
+invisible( file.copy(to_keep[ ,"kept_imgf"], newf))
+
+# Print status 
+msg <- paste0("Copied ", nrow(to_keep), " images into ", newf, "\n")
+cat(msg)
+
---
+++ mkpano.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# 
+# This file tries to make a panorama out of a folder of images
+# 
+
+function usage { 
+  echo "Usage $0 <inputfolder>"
+  exit 1
+}
+
+INFOLDER="$1" 
+shift 
+if [ -z "$INFOLDER" ]; then 
+  usage
+fi
+
+# Set proper options for rest of script 
+set -euox pipefail
+
+PROJ="project.pto"
+KEYCACHE="keypoints.cache"
+
+# 1: create the project file
+pto_gen -p 1 -o "$PROJ" $INFOLDER/*
+
+# 2: create control points 
+# Linear match with linear match length of 3
+# Cache keypoints 
+# Work on full scale data as the pictures are small (1920*1080)
+cpfind \
+  --linearmatch --linearmatchlen 3 \
+  -c --keypath "$KEYCACHE" \
+  --fullscale \
+  -o "$PROJ" "$PROJ"
+
+# 3: Set optimization parameters
+# ------------------------------
+
+# Unlink all images views (because height above seafloor can vary)
+NIMGS="$(find "$INFOLDER" -type f | wc -l)" 
+# for (( N=0; N<$NIMGS; N++ )); do 
+#   pto_var --unlink "v$N" -o "$PROJ" "$PROJ"
+# done
+  
+# 4: Optimize panorama 
+# --------------------
+# Optimise view except first, trX, trY
+# Set aside the list of control points
+./use_ctrpts.R "$PROJ" "scan"
+for (( N=1; N<$NIMGS; N++ )); do 
+  imgn=$N
+  imgp=$(( N - 1 ))
+  pto_var --opt "TrX$imgn,TrY$imgn" -o "$PROJ" "$PROJ"
+  # Use only control points of the pair of images 
+  ./use_ctrpts.R "$PROJ" "$imgp" "$imgn"
+  autooptimiser -n -o "$PROJ" "$PROJ" || true
+done 
+
+# Now use all control points, and refit the whole panorama
+./use_ctrpts.R "$PROJ" "all"
+pto_var --opt "TrX,TrY,TrZ" -o "$PROJ" "$PROJ"
+autooptimiser -n -o "$PROJ" "$PROJ"
+
+# 3: Cleanup control points 
+# -------------------------
+# echo "Cleaning up control points..." 
+# cpclean -s --max-distance=2 -o "$PROJ" "$PROJ"
+
+# 4: Refit the panorama after cleanup
+# -----------------------------------
+# pto_var --opt "TrX,TrY,TrZ" -o "$PROJ" "$PROJ"
+# autooptimiser -n -o "$PROJ" "$PROJ"
+
+
